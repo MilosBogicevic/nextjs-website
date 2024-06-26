f0f1813dbd412aac930bd11e506d9e1904324c69
unknown <milosbviser@gmail.com>
2024-06-26 22:56:19 +0200

    Created InfoSectionBelowHero component and applied style changes to Hero component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import homeImg from "/public/hero-image-1.jpg";
+import InfoSectionBelowHero from "@/components/InfoSectionBelowHero";
 
 export default function Home() {
   return (
@@ -8,6 +9,14 @@ export default function Home() {
         imgData={homeImg}
         imgAlt="Hero image for home page"
         title="We offer high performance eCommerce solutions"
+      />
+      <InfoSectionBelowHero
+        title="About Us"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          reprehenderit voluptatem magni laborum commodi? Facere quae nisi
+          laboriosam ut earum? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Obcaecati culpa voluptatibus, sequi illo sunt ad.
+          Est odit laborum autem a."
       />
     </>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ interface HeroProps {
 export default function Hero(props: HeroProps) {
   return (
     <>
-      <div className="relative h-screen flex justify-center items-center">
+      <div className="relative h-[80vh] flex justify-center items-center">
         <div className="absolute -z-10 inset-0">
           <Image
             src={props.imgData}
@@ -20,8 +20,8 @@ export default function Hero(props: HeroProps) {
           />
           <div className="absolute inset-0 bg-black opacity-60"></div>
         </div>
-        <div className="flex justify-center items-center">
-          <h1 className="text-white text-6xl max-w-[800px]">{props.title}</h1>
+        <div className="px-10 text-center md:text-left flex justify-center items-center">
+          <h1 className="text-white text-3xl sm:text-5xl md:text-6xl max-w-[800px]">{props.title}</h1>
         </div>
       </div>
     </>
