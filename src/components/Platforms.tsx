a61d07c81f469f28b2bd67a9715902c623d823c7
unknown <milosbviser@gmail.com>
2024-07-20 22:17:28 +0200

    Created JSON file with platforms data. Replaced repetitive data with a .map().

--- src/components/Platforms.tsx
+++ src/components/Platforms.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Logo from "/public/logo.svg";
 import SectionHeading from "./SectionHeading";
+import PlatformsData from "@/app/data/platforms.json";
 
 export default function Platforms() {
   return (
@@ -8,33 +8,17 @@ export default function Platforms() {
       <div className="flex flex-col items-center gap-y-20 px-4 text-center">
         <SectionHeading title="Technology platforms" />
         <div className="flex justify-center gap-8 flex-wrap">
-          <div>
-            <Image
-              src={Logo}
-              alt=""
-              width={200}
-              height={50}
-              className="transition-transform hover:scale-105"
-            />
-          </div>
-          <div>
-            <Image
-              src={Logo}
-              alt=""
-              width={200}
-              height={50}
-              className="transition-transform hover:scale-105"
-            />
-          </div>
-          <div>
-            <Image
-              src={Logo}
-              alt=""
-              width={200}
-              height={50}
-              className="transition-transform hover:scale-105"
-            />
-          </div>
+          {PlatformsData.map((platform, index) => (
+            <div key={index}>
+              <Image
+                src={platform.image}
+                alt=""
+                width={platform.width}
+                height={platform.height}
+                className="transition-transform hover:scale-105"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
